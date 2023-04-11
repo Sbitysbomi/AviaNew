ade9c07f9da5499d13123d02484b14eeb0e2b115
Sbitysbomi <daniilbokov4209@gmail.com>
2023-04-11 21:41:42 +0300

    void commit

--- src/components/FilterStops.js
+++ src/components/FilterStops.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Checkbox, FormControlLabel, FormGroup, FormLabel, Paper} from "@mui/material";
 
-function FilterStops(props) {
+function FilterStops() {
   return (
     <Paper sx={{p:2}}>
       <FormGroup>
